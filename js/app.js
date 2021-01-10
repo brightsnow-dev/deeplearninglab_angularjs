48386bdca27fd67ec4033dffca6f1f76b7669318
DiabateJS <diabatejs@yahoo.fr>
2021-01-10 19:53:31 +0100

    Transformation en application SPA

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-var app = angular.module('app',[]);
+var app = angular.module('app',[
+    'ngRoute'
+]);
+app.config(['$routeProvider', function config($routeProvider){
+    $routeProvider.
+        when('/home',{
+            templateUrl: 'views/home.html'
+        }).
+        when('/networks',{
+            templateUrl: 'views/networks.html',
+            controller: 'NetworkCtrl'
+        }).
+        when('/learningbase',{
+            templateUrl: 'views/learningbase.html',
+            controller: 'LearningBaseCtrl'
+        }).
+        when('/training',{
+            templateUrl: 'views/training.html',
+            controller: 'TrainingCtrl'
+        }).
+        otherwise('/home');
+}])
