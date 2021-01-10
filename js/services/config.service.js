45d0e7867c8655cae0af0443a78a59777be310e8
DiabateJS <diabatejs@yahoo.fr>
2021-01-10 20:01:19 +0100

    Utilisation du localhost dans le service de config.

--- js/services/config.service.js
+++ js/services/config.service.js
@@ -2,7 +2,7 @@ app.factory('Config', function(){
     return {
         name: 'DeeplearningLab',
         version: '1.0.0',
-        urlBack: 'http://djstechnologies.fr/deeplearninglab_api/index.php?',
+        urlBack: 'http://localhost:85/deeplearninglab_api/index.php?',
         SUCESS: 'SUCESS',
         ERROR: 'ERROR',
         WARNING: 'WARNING'
